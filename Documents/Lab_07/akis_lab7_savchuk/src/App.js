47d96ad1b60884af166939ef8a9306e7c732e459
NazarSavchuk <nazarsavcuk3@gmail.com>
2022-01-18 13:51:28 +0200

    first commit

---
+++ Documents/Lab_07/akis_lab7_savchuk/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { CFooter } from "./components/CFooter";
+import CHeader from "./components/CHeader";
+import CContents from "./components/CContents";
+import COuter from "./components/COuter";
+
+function App() {
+  return (
+    <div className="App">
+      <CHeader aName="Jan" aFamily="Kowalski" />
+      <CContents />
+      <COuter />
+      <CFooter weather="słoneczna" temp="25">
+        <p>Niebo jest bezchmurne, wiatr słaby.</p>
+      </CFooter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Documents/Lab_07/akis_lab7_savchuk/src/components/CFooter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./CFooter.css";
+
+export const CFooter = (props) => {
+  const { weather, temp, children } = props;
+
+  let date = new Date();
+  return (
+    <div>
+      <h5>
+        PPFront-end, przykładowy serwis React. Dzisiaj mamy:{" "}
+        {date.toDateString()}
+        <br />
+        Stan pogody: {weather}, temperatura: {temp}
+        <br />
+        {children}
+      </h5>
+    </div>
+  );
+};
+
+// export const CFooter = () => {
+//     let date = new Date();
+//     return React.createElement('div', {className: 'footerDivClass'},
+//       React.createElement('h5', {id: 'footerText', className: 'footerTextClass'},
+//       'PPFront-end, przykładowy serwis React. Dzisiaj mamy: '+date.toDateString()
+//       )
+//     )
+// }
+
+// export default CFooter;
